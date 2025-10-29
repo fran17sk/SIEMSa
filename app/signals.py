16c713b43ac40566a1a500c54b4b9252d797741c
fran17sk <francis_cruz2@hotmail.com.ar>
2025-10-29 11:10:21 -0300

    update

--- app/signals.py
+++ app/signals.py
@@ -1,4 +1,3 @@
-# signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -6,9 +5,14 @@ from .models import PerfilUsuario
 
 @receiver(post_save, sender=User)
 def crear_perfil_usuario(sender, instance, created, **kwargs):
+    """Crea un PerfilUsuario automáticamente cuando se crea un nuevo User."""
     if created:
         PerfilUsuario.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def guardar_perfil_usuario(sender, instance, **kwargs):
-    instance.perfilusuario.save()
+    """Guarda el perfil cuando se actualiza el User."""
+    try:
+        instance.perfilusuario.save()
+    except PerfilUsuario.DoesNotExist:
+        PerfilUsuario.objects.create(user=instance)
